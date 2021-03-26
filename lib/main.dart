605b05330fc4bb9ac6b7efb6c5061850cb37f9ca
keerthanenr <keerthanenr@live.co.uk>
2021-03-26 16:23:41 +0000

    Button and Dices added to body

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,25 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(children: <Widget>[
+        Expanded(
+          child: TextButton(
+            onPressed: () {
+              print('Left Button pressed now!');
+            },
+            child: Image.asset('images/dice1.png'),
+          ),
+        ),
+        Expanded(
+          child: TextButton(
+            onPressed: () {
+              print('Right Button pressed now!');
+            },
+            child: Image.asset('images/dice2.png'),
+          ),
+        ),
+      ]),
+    );
   }
 }
