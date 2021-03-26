7a8541a44eb32a888619563e6c9ca307fe45a8e5
keerthanenr <keerthanenr@live.co.uk>
2021-03-26 19:05:20 +0000

    Commiting final state of the dart app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,7 +16,22 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var leftDiceNumber = 4;
+  var rightDiceNumber = 3;
+
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -23,17 +39,22 @@ class DicePage extends StatelessWidget {
         Expanded(
           child: TextButton(
             onPressed: () {
+              setState(() {
+                changeDiceFace();
+              });
               print('Left Button pressed now!');
             },
-            child: Image.asset('images/dice1.png'),
+            child: Image.asset('images/dice$leftDiceNumber.png'),
           ),
         ),
         Expanded(
           child: TextButton(
             onPressed: () {
-              print('Right Button pressed now!');
+              setState(() {
+                changeDiceFace();
+              });
             },
-            child: Image.asset('images/dice2.png'),
+            child: Image.asset('images/dice$rightDiceNumber.png'),
           ),
         ),
       ]),
